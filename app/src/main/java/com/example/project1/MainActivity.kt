4f04bfc34f1da82abed42ebb1830296306895f57
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2022-11-22 00:14:57 +0200

    updateEtn1

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.*
-import androidx.annotation.ContentView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
+
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
 import java.text.DateFormat
@@ -21,21 +20,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         val bounceAnimation = AnimationUtils.loadAnimation(applicationContext,R.anim.animation)
-
+        var numberOfTickets : Int = 1
 
         val Theaters = resources.getStringArray(R.array.Theaters)
 
         val numberOfTicketsNumberPicker = findViewById<NumberPicker>(R.id.numberOfTickets)
-        numberOfTicketsNumberPicker.minValue = 0
-        numberOfTicketsNumberPicker.maxValue = 10   //max number of tickets
 
-        numberOfTicketsNumberPicker.setDisplayedValues(arrayOf<String>("0","1","2","3","4","5","6","7","8","9","10"));
+        numberOfTicketsNumberPicker.minValue = 1
+        numberOfTicketsNumberPicker.maxValue = 10
+        numberOfTicketsNumberPicker.wrapSelectorWheel = true
         numberOfTicketsNumberPicker.visibility = View.INVISIBLE
-
-        numberOfTicketsNumberPicker.isKeyboardNavigationCluster = false
 
         val Datetextview = findViewById<TextView>(R.id.textView3)
         val Theatertextview = findViewById<TextView>(R.id.textView2)
@@ -48,15 +43,26 @@ class MainActivity : AppCompatActivity() {
         val SumTextView = findViewById<TextView>(R.id.textView5)
 
         SumTextView.visibility = View.INVISIBLE
+        numberOfTicketsNumberPicker.setOnValueChangedListener(NumberPicker.OnValueChangeListener { picker, oldVal, newVal ->
+            numberOfTickets = newVal
+            SumTextView.text = "Sum: " + (newVal * 10).toString() + " $"
+        })
+        showhidebtn.setOnClickListener{
+            Datetextview.visibility = View.VISIBLE
+            Theatertextview.visibility = View.VISIBLE
+            Theaterspinner.visibility = View.VISIBLE
+            Datespinner.visibility = View.VISIBLE
+            buynow.visibility = View.VISIBLE
+            ChooseNumberOfTicketstextview.visibility = View.VISIBLE
+            numberOfTicketsNumberPicker.visibility = View.VISIBLE
+            SumTextView.visibility = View.VISIBLE
+            SumTextView.text = getString(R.string.sum) + (numberOfTickets * 10).toString()+ "$"
+        }
 
         buynow.setOnClickListener{
-            if ((numberOfTicketsNumberPicker.value.toInt()).toString() == "0") {
-                Toast.makeText(this, "Please choose number of tickets", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                val toast = Toast.makeText(this, "Thank you for buying tickets", Toast.LENGTH_SHORT)
-                toast.setGravity(Gravity.CENTER, 0, 0)
-                toast.show()
+
+            val toast = Toast.makeText(this, "Thank you for buying tickets", Toast.LENGTH_SHORT)
+            toast.show()
 
             val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val popupView: View = inflater.inflate(R.layout.purchase_confirmation, null)
@@ -71,15 +77,15 @@ class MainActivity : AppCompatActivity() {
             popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0)
 
             val purchaseTextView = popupWindow.contentView.findViewById<TextView>(R.id.Purchase)
-
-            purchaseTextView.text = getString(R.string.purchase) + "\n"  + getString(R.string.purchase2) + " " + (numberOfTicketsNumberPicker.value.toInt() - 1).toString() + " " + getString(R.string.purchase3)+ " " + (Theaterspinner.selectedItem).toString()  + " " + getString(R.string.purchase4)+ " " + (Datespinner.selectedItem).toString() +  " " +  " " + getString(R.string.purchase5) + " " + ((numberOfTicketsNumberPicker.value.toInt() - 1) * 10).toString() + "$ " +  getString(R.string.purchase6)
+            numberOfTickets = numberOfTicketsNumberPicker.value
+            purchaseTextView.text = getString(R.string.purchase) + "\n"  + getString(R.string.purchase2) + " " + (numberOfTickets).toString() + " " + getString(R.string.purchase3)+ " " + (Theaterspinner.selectedItem).toString()  + " " + getString(R.string.purchase4)+ " " + (Datespinner.selectedItem).toString() +  " " +  " " + getString(R.string.purchase5) + " " + (numberOfTickets * 10).toString() + "$ " +  getString(R.string.purchase6)
             // dismiss the popup window when touched
             popupView.setOnTouchListener { v, event ->
                 popupWindow.dismiss()
                 true
             }
         }
-        }
+
 
 
         var PhotoVar = findViewById<ImageView>(R.id.imageView1)
@@ -91,16 +97,7 @@ class MainActivity : AppCompatActivity() {
 
         showhidebtn.startAnimation(bounceAnimation)
 
-        showhidebtn.setOnClickListener{
-            Datetextview.visibility = View.VISIBLE
-            Theatertextview.visibility = View.VISIBLE
-            Theaterspinner.visibility = View.VISIBLE
-            Datespinner.visibility = View.VISIBLE
-            buynow.visibility = View.VISIBLE
-            ChooseNumberOfTicketstextview.visibility = View.VISIBLE
-            numberOfTicketsNumberPicker.visibility = View.VISIBLE
-            SumTextView.visibility = View.VISIBLE
-        }
+
         if (Theaterspinner != null) {
             val Theateradapter = ArrayAdapter(
                 this,
@@ -123,10 +120,6 @@ class MainActivity : AppCompatActivity() {
                     // write code to perform some action
                 }
             }
-        numberOfTicketsNumberPicker.setOnValueChangedListener { numberPicker, i, i2 ->
-            val valuepicker = numberOfTicketsNumberPicker.value.toInt() - 1
-            SumTextView.text = getString(R.string.sum) + (valuepicker * 10).toString()+ "$"
-        }
 
 
 
